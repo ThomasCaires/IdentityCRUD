abe3bb90cdcdac20ac9a3e086d96b35fbc76abba
Thomas Caires <thomas_cairesaraujo@outlook.com>
2023-09-27 01:48:08 -0300

    Add project files.

---
+++ IdentityCRUD/Entities/Aluno.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityCRUD.Entities
+{
+    public class Aluno
+    {
+        public int AlunoId { get; set; }
+
+        [Required, MaxLength(80, ErrorMessage = "Nome não pode exder 80 caracteres")]
+        public string? Nome { get; set; }
+
+        [EmailAddress]
+        [Required, MaxLength(120)]
+        public string? Email { get; set; }
+
+        public int Idade { get; set; }
+
+        [MaxLength(80)]
+        public string? Curso { get; set; }
+    }
+}
