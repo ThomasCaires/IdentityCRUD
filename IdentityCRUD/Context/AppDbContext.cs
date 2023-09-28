5667e181575caeb8ee963fe091d916461100ff75
Thomas Caires <thomas_cairesaraujo@outlook.com>
2023-09-28 00:22:23 -0300

    Data base e controller cirados

---
+++ IdentityCRUD/Context/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using IdentityCRUD.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace IdentityCRUD.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Aluno>().HasData(
+                new Aluno
+                {
+                    AlunoId = 1,
+                    Nome = "Thomas",
+                    Email = "thomas@outlook.com",
+                    Idade = 21,
+                    Curso = "Sistemas de informação"
+                });
+        }
+    }
+}
--- IdentityCRUD/Program.cs
+++ IdentityCRUD/Program.cs
@@ -1,7 +1,15 @@
+using IdentityCRUD.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Obtendo a string de connection
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
 
 var app = builder.Build();
 
